1c93b5f6b2ef47959021c74148f9ecaaacda2a03
developerakshat12 <148519895+developerakshat12@users.noreply.github.com>
2025-08-11 23:51:40 +0530

    Time: 12 ms (26.39%), Space: 132.9 MB (38.29%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        //so what we want is - 
+        //alternate + and - in the array
+        //order of the array should be mainteained 
+        //checking if the array begins with positive or negative 
+        //STEP 1 - alternate + and - 
+        int n = nums.size();
+        vector<int> ans (n);
+        // this here will be our answer vector 
+        //NOTE - Never change the original vector in interview it is not a good practice as in real life we wont be changing the input but manipulating it and getting the desired output 
+        //**IDEATION for the problem 1 
+        //2 -4 1 2 4 -5 -3 -5
+        //point one as read (0)  and other as finder and swaper (1) 
+        //2 check the <0 number in ith position using finder
+        //swap with swapper 
+        // read += 2 
+        //swapper += (read +1);
+        //this solves first two things**
+
+        //cant swap violates the 2nd rule 
+        //swap will not happen it will be pushed into the 
+
+
+        // lol we can just create vec positive and vec negative and combine them to form the answer 
+        vector<int> pos (n/2);
+        vector<int> neg (n/2);
+        int positive_pos = 0;
+        int negative_pos = 0;
+        for(int i = 0; i < n ; i++){
+            if(nums[i] > 0) pos[positive_pos++] = nums[i];
+            else neg[negative_pos++] = nums[i];
+        }
+
+        for(int j = 0; j < n; j++){
+            if(j%2 == 0) ans[j] = pos[j/2];
+            else ans[j] = neg[j/2];
+        }
+
+        return ans;
+    }
+};
